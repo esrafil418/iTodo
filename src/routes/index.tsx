5261d7f93a290fefad783903179d82d389bc535d
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-02 22:44:09 +0330

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3 className="bg-red-600">سلام</h3>
+    </div>
+  );
+}
