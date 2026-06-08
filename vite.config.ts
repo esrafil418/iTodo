eb752a7bc794e8e43761f7e50587a0af5f0a1877
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-09 00:00:46 +0330

    fix: update packages to fix deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
 	plugins: [
 		tanstackRouter({
